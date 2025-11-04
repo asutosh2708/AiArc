c99a7cb25b5f3282b71f87f301d35d03d66a54e7
Asutosh Das <asutoshdas2708@gmail.com>
2025-11-04 22:35:36 +0530

    Minor changes to main.py

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI, HTTPException
 
 app=FastAPI()
 
-items = ['apple','orange']
+items = ["apple", "orange"]
 
 @app.get("/")
 def root():
-    return {"Hello": "World"}
+    return {"Hello":"World!"}
 
 @app.post("/items")
-def create_item(item: str):
+def create_item(item:str):
     items.append(item)
     return items
 
@@ -18,4 +18,4 @@ def get_item(item_id: int) -> str:
     if item_id<len(items):
         return items[item_id]
     else:
-        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+        raise HTTPException(status_code=404, detail=f"Item {item_id} Not Found!")
